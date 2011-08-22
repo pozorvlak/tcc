0bca20ac076c2663ef41543966f36bb3ad07c838
Miles Gould <miles@assyrian.org.uk>
2011-08-22 21:44:18 +0100

    Write out asm file that returns zero.

--- tcc.c
+++ tcc.c
@@ -3,6 +3,21 @@
 #include <errno.h>
 
 #define SFILE_NAME "ignore_me.s"
+char *asm_boilerplate =
+"	.file	\"zero.c\"\n"
+"	.text\n"
+".globl main\n"
+"	.type	main, @function\n"
+"main:\n"
+"	pushl	\%ebp\n"
+"	movl	\%esp, \%ebp\n"
+"	movl	$0, \%eax\n"
+"	popl	\%ebp\n"
+"	ret\n"
+"	.size	main, .-main\n"
+"	.ident	\"TCC 0.0.1\"\n"
+"	.section	.note.GNU-stack,\"\",@progbits\n"
+;
 
 int fcall(int succeeded, char *error_msg, int exit_code)
 {
@@ -19,7 +34,7 @@ int main(char argc, char** argv)
 	
 	fcall((int) (sfile = fopen(SFILE_NAME, "w")),
 		"Couldn't open input file", 1);
-	fprintf(sfile, "Hello, world!\n");
+	fprintf(sfile, "%s", asm_boilerplate);
 	fcall(!fclose(sfile), "Couldn't close input file", 2);
 	return 0;
 }
