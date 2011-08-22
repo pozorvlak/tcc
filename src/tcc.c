3f73c7bbf3bc3a35d16861a27216d28c5197bf7e
Miles Gould <miles@assyrian.org.uk>
2011-08-23 00:02:08 +0100

    Strip whitespace and comments, then read an integer from infile.

--- src/tcc.c
+++ src/tcc.c
@@ -29,6 +29,49 @@ int fcall(int succeeded, char *error_msg, int exit_code)
 	return succeeded;
 }
 
+int read_integer(FILE *stream)
+{
+	int i = 0;
+	int c;
+	while (c = fgetc(stream)) {
+		if (isdigit(c)) {
+			i *= 10;
+			i += (c - '0');
+		} else {
+			ungetc(c, stream);
+			break;
+		}
+	}
+	return i;
+}
+
+void discard_line(FILE *stream)
+{
+	int c = 0;
+	do {
+		c = fgetc(stream);
+	} while (c != '\n');
+}
+
+void read_whitespace(FILE *stream)
+{
+	int c;
+	while (c = fgetc(stream)) {
+		if (c == '/') {
+			int c2 = fgetc(stream);
+			if (c2 == '/') {
+				discard_line(stream);
+			} else {
+				ungetc(c2, stream);
+				ungetc(c, stream);
+			}
+		} else if (!isspace(c)) {
+			ungetc(c, stream);
+			break;
+		}
+	}
+}
+
 int main(char argc, char** argv)
 {
 	FILE *cfile, *sfile;
@@ -36,10 +79,13 @@ int main(char argc, char** argv)
 	fcall(argc == 2, "I can only handle one argument for now", 3);
 	cfile = fopen(argv[1], "r");
 	fcall((int) cfile, "Couldn't open input file", 4);
+	read_whitespace(cfile);
+	int retval = read_integer(cfile);
 	fcall((int) (sfile = fopen(SFILE_NAME, "w")),
 		"Couldn't open output file", 1);
 	fprintf(sfile, "%s", asm_boilerplate_start);
-	fprintf(sfile, "%s", "\tmovl\t$0,\t\%eax\n");
+	fprintf(sfile, "\tmovl\t$%d,\t%%eax\n", retval);
+	printf("\tmovl\t$%d,\t%%eax\n", retval);
 	fprintf(sfile, "%s", asm_boilerplate_end);
 	fcall(!fclose(sfile), "Couldn't close output file", 2);
 	return 0;
