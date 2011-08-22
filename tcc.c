41b41058b801174463a077a89f1987f11deb19a8
Miles Gould <miles@assyrian.org.uk>
2011-08-22 20:29:32 +0100

    Hello, world file-writer: crashes GNU ld 2.20.1-system.20100303

---
+++ tcc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SFILE_NAME "ignore_me.s"
+
+extern int errno;
+
+int fcall(int succeeded, char *error_msg, int exit_code)
+{
+	if (!succeeded) {
+		fprintf(stderr, "ERROR: %s, code %i\n", error_msg, errno);
+		exit(exit_code);
+	}
+	return succeeded;
+}
+
+int main(char argc, char** argv)
+{
+	FILE *sfile;
+	sfile = fopen(SFILE_NAME, "w");
+	fcall((int) sfile, "Couldn't open input file", 1);
+	fprintf(sfile, "Hello, world!\n");
+	fcall(fclose(sfile), "Couldn't close input file", 2);
+}
+
